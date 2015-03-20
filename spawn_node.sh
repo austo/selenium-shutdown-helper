a7a4239cd64aba7e77efdaa70d5847fb709d348f
Austin Moran <austin.moran@surveysampling.com>
2015-03-20 17:05:13 -0400

    trap int/term and exit

--- spawn_node.sh
+++ spawn_node.sh
@@ -3,10 +3,23 @@
 MAIN_CLASS='org.openqa.grid.selenium.GridLauncher'
 HUB_REG_URL='http://localhost:4444/grid/register'
 SHUTDOWN_SERVLET='com.moraustin.NodeShutdownServlet'
+NODE_CONFIG='../config/nodeConfig.json'
+SLEEP_INTERVAL='10'
+
+SHOULD_RUN=true
+
+kill_node() {
+  SHOULD_RUN=false
+  kill 0
+  wait
+}
+
+trap kill_node TERM INT
 
 cd bin
 
-while true; do
-    java -cp *:. ${MAIN_CLASS} -role node -hub ${HUB_REG_URL} -servlets ${SHUTDOWN_SERVLET} -nodeConfig ../config/nodeConfig.json
-    sleep 20
+while ${SHOULD_RUN}; do
+    java -cp *:. ${MAIN_CLASS} -role node -hub ${HUB_REG_URL} -servlets ${SHUTDOWN_SERVLET} -nodeConfig ${NODE_CONFIG} &
+    wait
+    sleep ${SLEEP_INTERVAL}
 done
