37b800b91fbd0a6a16557893b54e70ad31192692
Austin Moran <austin.moran@surveysampling.com>
2015-04-17 11:04:44 -0400

    add resources file to deployment package

--- package.sh
+++ package.sh
@@ -3,12 +3,13 @@
 DEPLOY_ARCHIVE='deploy.tar.gz'
 DEPLOY_DIR='deploy'
 
-printf "Creating deployment archive at $(tput bold)$(tput setaf 2)%s$(tput sgr0)\n" "$DEPLOY_ARCHIVE"
-rm -f $DEPLOY_ARCHIVE
-rm -fr $DEPLOY_DIR
-mkdir -p $DEPLOY_DIR
+printf "Creating deployment archive at $(tput bold)$(tput setaf 2)%s$(tput sgr0)\n" "${DEPLOY_ARCHIVE}"
+rm -f ${DEPLOY_ARCHIVE}
+rm -fr ${DEPLOY_DIR}
+mkdir -p "${DEPLOY_DIR}/src/main"
 cp -fR bin "${DEPLOY_DIR}/"
+cp -fR src/main/resources "${DEPLOY_DIR}/src/main"
 cp -fR config "${DEPLOY_DIR}/"
 cp run_hub.sh run_node.sh "${DEPLOY_DIR}/"
-tar czvf $DEPLOY_ARCHIVE $DEPLOY_DIR
-rm -fr $DEPLOY_DIR
+tar czvf ${DEPLOY_ARCHIVE} ${DEPLOY_DIR}
+rm -fr ${DEPLOY_DIR}
